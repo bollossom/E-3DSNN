12350eccd71c105f34ac71dbfb0d3df8ff8ce073
bollossom <2223453260@qq.com>
2025-07-07 11:26:36 +0800

    add det

---
+++ det/pcdet/models/backbones_2d/__init__.py
@@ -0,0 +1,8 @@
+from .base_bev_backbone import BaseBEVBackbone, BaseBEVBackboneV1, BaseBEVResBackbone,BaseBEVBackbone_spike
+
+__all__ = {
+    'BaseBEVBackbone': BaseBEVBackbone,
+    'BaseBEVBackboneV1': BaseBEVBackboneV1,
+    'BaseBEVResBackbone': BaseBEVResBackbone,
+    'BaseBEVBackbone_spike':BaseBEVBackbone_spike,
+}
